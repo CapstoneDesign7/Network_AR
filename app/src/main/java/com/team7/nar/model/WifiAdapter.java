e35eb668612541294b42cd895bda6b1b260d3c29
jisoochoi7561 <jisoochoi7561@gmail.com>
2022-05-23 03:33:22 +0900

    jisoo wifi&main resources

---
+++ app/src/main/java/com/team7/nar/model/WifiAdapter.java
@@ -0,0 +1,54 @@
+package com.team7.nar.model;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.team7.nar.R;
+
+import java.util.ArrayList;
+
+
+public class WifiAdapter extends RecyclerView.Adapter<WifiAdapter.ViewHolder> {
+    ArrayList<WiFi> wifis = new ArrayList<WiFi>();
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemview = inflater.inflate(R.layout.wifi_item,parent,false);
+        return new ViewHolder(itemview);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        WiFi wifi = wifis.get(position);
+        holder.setItem(wifi);
+    }
+
+    @Override
+    public int getItemCount() {
+        return wifis.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView wifiName;
+        TextView wifiSignal;
+        TextView wifiSpeed;
+        public ViewHolder(View itemview){
+            super(itemview);
+
+            wifiName = itemview.findViewById(R.id.wifi_name);
+            wifiSignal = itemview.findViewById(R.id.wifi_signal);
+            wifiSpeed= itemview.findViewById(R.id.wifi_speed);
+        }
+
+        public void setItem(WiFi wifi){
+            wifiName.setText(wifi.getName());
+            wifiSignal.setText(wifi.getRssiLevel());
+            wifiSpeed.setText(wifi.getLinkSpeed());
+        }
+    }
+}
--- app/src/main/java/com/team7/nar/view/WiFiList.java
+++ app/src/main/java/com/team7/nar/view/WiFiList.java
@@ -1,4 +1,29 @@
 package com.team7.nar.view;
 
-public class WiFiList {
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.team7.nar.R;
+import com.team7.nar.model.WifiAdapter;
+
+public class WiFiList extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wifi_recyclerview);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        WifiAdapter adapter = new WifiAdapter();
+        // add live data to here
+        recyclerView.setAdapter(adapter);
+        // Junyoung
+        // Injae
+    }
 }
