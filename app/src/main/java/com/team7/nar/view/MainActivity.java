39fce77fb73db1476f408efe0f4559b5947fcb91
Injae <alfkalfk1234@naver.com>
2022-05-24 14:01:20 +0900

    integration

--- app/src/main/java/com/team7/nar/view/MainActivity.java
+++ app/src/main/java/com/team7/nar/view/MainActivity.java
@@ -27,7 +27,11 @@ public class MainActivity extends AppCompatActivity {
     String[] permission_list = {
             Manifest.permission.CAMERA,
             Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE
     };
 
     @Override
