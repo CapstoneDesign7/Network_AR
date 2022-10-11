f7b295f00259b40772596615ad0ab5512068a3d3
Junyoung <wnsdud12365@naver.com>
2022-10-11 16:07:02 +0900

    Title Splash 추가

---
+++ app/src/main/java/com/team7/nar/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.team7.nar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView intro = (ImageView) findViewById(R.id.intro);
+        Glide.with(this).load(R.drawable.title).into(intro);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent main = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(main);
+                finish();
+            }
+        }, 2000);
+    }
+}
